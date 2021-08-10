bbef9fb963647655ed68680109cfb2eb2c7130c5
unknown <huangweieric@gmail.com>
2021-08-10 12:24:33 +0400

    add response.go go.mod

---
+++ go.mod
@@ -0,0 +1,3 @@
+module study.com/eric/test-import-part2
+
+go 1.16
---
+++ myapi/response.go
@@ -0,0 +1,12 @@
+package myapi
+
+import "fmt"
+
+var msg string
+var mymap map[string]interface{}
+
+func Res()  {
+
+	fmt.Println(msg,mymap)
+
+}
